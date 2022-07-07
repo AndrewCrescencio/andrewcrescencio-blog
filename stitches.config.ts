30e61b76ed221e82044662312a10c6669ec2e0da
AndrewCrescencio <andrew_workstation@hotmail.com>
2022-07-07 04:17:31 -0300

    :lipstick: feat(stitches.config.ts): add new @media breakpoints

--- stitches.config.ts
+++ stitches.config.ts
@@ -38,6 +38,12 @@ export const { styled, getCssText, createTheme, globalCss } = createStitches({
       7: '5.6rem',
       8: '6.4rem',
     },
+    media: {
+      bp1: '(min-width: 414px)',
+      bp2: '(min-width: 768px)',
+      bp3: '(min-width: 1024px)',
+      bp4: '(min-width: 1440px)',
+    },
   },
 });
 
